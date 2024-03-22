c8ecdf2c748b007e56c492a4b7fb99ddcf52c3eb
Max <161768135+Picoodi@users.noreply.github.com>
2024-03-22 10:03:58 +0100

    Update Lorenzfactor_Special_Relativity_Theorie.py
    changing lifetime to 100

--- Lorenzfactor_Special_Relativity_Theorie.py
+++ Lorenzfactor_Special_Relativity_Theorie.py
@@ -24,15 +24,15 @@ def velocity():
     velocity_solution = math.sqrt(1-(1/gamma**2))*c
     print(velocity_solution)
     
- #function that calculates how much of a change of lifetime it would be for a human of 83 years in relativity to a system with factor 0   
+ #function that calculates how much of a change of lifetime it would be for a human of 100 years in relativity to a system with factor 0   
 def human():
     print("Your gamma plz:")
     gamma = float(input())
-    years = (gamma - 1)*83
-    days = (gamma - 1)*83*365
-    hours = (gamma - 1)*83*365*24
-    minutes = (gamma - 1)*83*365*24*60
-    seconds = (gamma - 1)*83*365*24*60*60
+    years = (gamma - 1)*100
+    days = (gamma - 1)*100*365
+    hours = (gamma - 1)*100*365*24
+    minutes = (gamma - 1)*100*365*24*60
+    seconds = (gamma - 1)*100*365*24*60*60
 
     print("The Human lifetime would change about:")
     print(years, "years or")
