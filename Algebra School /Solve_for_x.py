0801125523b5b98b5d05e18851ff940755d8d62a
Max <161768135+Picoodi@users.noreply.github.com>
2024-10-22 19:06:48 +0200

    Update Solve_for_x.py
    Adding the code and A comment

--- Algebra School /Solve_for_x.py
+++ Algebra School /Solve_for_x.py
@@ -1,1 +1,20 @@
+import sympy
+from sympy import *
 
+#The function takes an equation with a variable x and no else variables, solves after x and returns the solution
+def solve_for_x(equation):
+  x = symbols("x")
+  solution = solve(equation,x)
+  return solution
+
+
+
+
+
+#A little test code.
+#the equation x^2 -4 has the solutions x1 = -2 and x2 = 2.
+eq = x**2 -4
+if solve_for_x(eq) == [-2,2]:
+  print("Test passed, right solution calculated")
+else:
+  print("Test failed, calculated wrong")  
