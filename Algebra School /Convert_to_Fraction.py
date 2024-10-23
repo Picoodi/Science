e5782747b08f0518e47fd07a75e5f6feccb50e6e
Max <161768135+Picoodi@users.noreply.github.com>
2024-10-23 12:24:47 +0200

    Create Convert_to_Fraction.py

---
+++ Algebra School /Convert_to_Fraction.py
@@ -0,0 +1,22 @@
+#A function to convert decimals into a fraction which is stored as a tuple
+#eg for making solutions look better
+
+def decimals_to_fraction(decimal: str):
+  exponent = int(len(decimal))-1
+  number = float(decimal)
+  numerator = int(number*10*exponent)
+  denominator = 10*exponent
+
+return numerator, denominator
+
+
+
+#TEST CODE
+#x = 0.1 and y = 0.7 x+y= 0.8
+x= 0.1
+y= 0.7
+equation = x+y
+print(x+y)
+
+solution = decimals_to_fractions(str(equation))
+print(solution[0], "/", solution[1])
