1cf6de3e5164d8a61ad20530ece1fcb396f6874d
Max <161768135+Picoodi@users.noreply.github.com>
2024-11-17 18:40:18 +0100

    Update Convert_to_Fraction.py
    Updating the code and creating a nicer test function  aswell as making a better comment

--- Algebra School /Convert_to_Fraction.py
+++ Algebra School /Convert_to_Fraction.py
@@ -8,15 +8,6 @@ def decimals_to_fraction(decimal: str):
   return numerator, denominator
 
 
-
-def decimals_to_fraction(decimal: str):
-  exponent = int(len(decimal))-1
-  number = float(decimal)
-  numerator = int(number*10*exponent)
-  denominator = 10*exponent
-  return numerator, denominator
-
-
 #A test function
 def test(decimal, solution):      
     answer = decimals_to_fraction(str(decimal))
