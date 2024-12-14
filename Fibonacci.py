1a9283ace179c63aa16b91131b6e05ca1d08fc34
Max <161768135+Picoodi@users.noreply.github.com>
2024-12-14 18:49:19 +0100

    Create Fibonacci.py
    Adding the code for the sequenze and some test code at the bottom

---
+++ Fibonacci.py
@@ -0,0 +1,42 @@
+#Fibonacci Sequence
+def Fibonacci(n):
+    if n < 0:
+        print("Not a valid input range")
+    elif n == 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0,1]
+
+    else:
+        Fibonacci = [0,1]
+        for i in range(1,n):
+            Fibonacci.append(Fibonacci[i-1]+ Fibonacci[i])
+            
+            return Fibonacci
+
+
+  
+
+
+###Test#Code###
+if Fibonacci(0) == []:
+    print("Test 1 passed")
+else:
+    print("Test 1 failed")
+
+if Fibonacci(2) == [0,1]:
+    print("Test 2 passed")
+else:
+    print("Test 2 failed")
+
+if Fibonacci(4) == [0,1,1,2,3,5,8]:
+    print("Test 3 passed")
+else:
+    print("Test 3 failed")
+
+if Fibonacci(10) == [0,1,1,2,3,5,8,13,21,34]:
+    print("Test 4 passed")
+else:
+    print("Test 4 failed")
