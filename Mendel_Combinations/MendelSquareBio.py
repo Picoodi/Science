b0bd7dcbd4c364d944d6840df547dac4810b8e3c
Max <161768135+Picoodi@users.noreply.github.com>
2025-01-28 21:17:05 +0100

    Update MendelSquareBio.py

--- Mendel_Combinations/MendelSquareBio.py
+++ Mendel_Combinations/MendelSquareBio.py
@@ -1,21 +1,19 @@
-userinput = "ab"
+userinput = "abcdefghijk"
 
 def generate_combinations(letters):
         if not letters:
               return [""]
-        # Rekursive function for the rest of the letters
+                
         first_combis = generate_combinations(letters[1:])
 
-        # Solution list for new combinations
         combinations = []
-        
-        # Für jede Kombination aus der rekursiven Rückgabe füge den aktuellen Buchstaben hinzu
         for kombi in first_combis:
             combinations.append(letters[0].upper() + kombi)  # Append uppercase letters
             combinations.append(letters[0].lower() + kombi)  # Append lowercase letters
-
+                
         return combinations
 
+possible_combinations = 2**(len(userinput)*2)
 
 with open("Mendel_Kombinationsliste.txt", "w") as file:
     combinations =generate_combinations(userinput)
@@ -27,9 +25,6 @@ with open("Mendel_Kombinationsliste.txt", "w") as file:
             kombination = combinations[i] + combinations2[s]
             file.write(kombination +" ")
             s = s+1
-                    
         i = i+1
 
-
-combinations = 2**(len(userinput)*2)
-print("All",combinations, " combinations saved in Mendel_kombinationsliste.txt")
+print("All",possible_combinations, " combinations saved in Mendel_kombinationsliste.txt")
