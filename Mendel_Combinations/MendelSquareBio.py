7847828ca97635bc4ee6e06b7f16885cf30bb3eb
Max <161768135+Picoodi@users.noreply.github.com>
2025-02-05 20:08:53 +0100

    MendelSquareBio.py aktualisieren

--- Mendel_Combinations/MendelSquareBio.py
+++ Mendel_Combinations/MendelSquareBio.py
@@ -1,35 +1,49 @@
-userinput = "abc"
+from itertools import product
+from prettytable import PrettyTable
+table = PrettyTable()
 
 
-def generate_combinations(letters):
-        if not letters:
-              return [""]
-        # Rekursive function for the rest of the letters
-        first_combis = generate_combinations(letters[1:])
+def create_germ_cells(input_data):
+    #Changing input into groups like ("A,a", "B,b") too AB ab aB ab
+    groups = [group.split(",") for group in input_data]
 
-        # Solution list for new combinations
-        combinations = []
-        
-        # Für jede Kombination aus der rekursiven Rückgabe füge den aktuellen Buchstaben hinzu
-        for kombi in first_combis:
-            combinations.append(letters[0].upper() + kombi)  # Append uppercase letters
-            combinations.append(letters[0].lower() + kombi)  # Append lowercase letters
+    # With product we get all the possible combinations between the groups
+    combinations = product(*groups)
 
-        return combinations
+    #now we put all combos into a single list we return back
+    cells = []
+    for combo in combinations:
+        cells.append(combo)
+        cells = [''.join(tup) for tup in cells]
+
+    return cells
 
 
-possible_combinations = 2**(len(userinput)*2)
+def crossing(name, GC1,GC2):
+    #We create the germ cells for the Allels we pass
+    GC1 = create_germ_cells(GC1)
+    GC2 = create_germ_cells(GC2)
 
-with open("Mendel_Kombinationsliste.txt", "w") as file:
-    combinations =generate_combinations(userinput)
+    #The table.clear() is for the PrettyTable we create
+    #First we clear it and then while running the programm pass new colums into it
+    table.clear()
+    table.add_column(name, GC2)
+
+    solution = []
     i = 0
-    while i <len(combinations):
+    while i < len(GC1):
         s = 0
-        while s < len(combinations):
-            kombination = combinations[i] + combinations[s]
-            file.write(kombination +" ")
+        #Tablelist is for storing data for a colum 
+        Tablelist = []
+
+        while s < len(GC2):
+            kombination = GC1[i] + GC2[s]
+            solution.append(kombination)
+            Tablelist.append(kombination)
             s = s+1
 
+        table.add_column(GC1[i], Tablelist)
         i = i+1
+    print(table)
 
-print("All",possible_combinations, " combinations saved in Mendel_kombinationsliste.txt")
+    return solution
